c1a99d458a181af3d34d32d6a3ccb4dcdba795bb
Jurian de Cocq van Delwijnen <jurian.delwijnen@sogeti.com>
2017-11-01 19:28:57 +0100

    Trigger

--- src/main/java/nl/sogeti/bootcamp/Example.java
+++ src/main/java/nl/sogeti/bootcamp/Example.java
@@ -4,6 +4,7 @@ public class Example {
 
     // Added some comments to trigger a new build
     // Some more changes!
+    // Trigger compile job
     public String getAbcd() {
         return "ABCD";
     }
