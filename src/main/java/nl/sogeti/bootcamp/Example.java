c5ea621419e87077cd982a10885b270fb317cbd8
Jurian de Cocq van Delwijnen <jurian.delwijnen@sogeti.com>
2017-11-01 19:24:54 +0100

    More Changes!

--- src/main/java/nl/sogeti/bootcamp/Example.java
+++ src/main/java/nl/sogeti/bootcamp/Example.java
@@ -3,6 +3,7 @@ package nl.sogeti.bootcamp;
 public class Example {
 
     // Added some comments to trigger a new build
+    // Some more changes!
     public String getAbcd() {
         return "ABCD";
     }
